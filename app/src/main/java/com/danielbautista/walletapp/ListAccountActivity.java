69aba1435aaa2ef8441851ba8cd054a0f092049a
DanielBautistaMr <dbautista542@unab.edu.co>
2023-09-08 17:50:04 -0500

    Agregar datos de la cuenta

---
+++ app/src/main/java/com/danielbautista/walletapp/ListAccountActivity.java
@@ -0,0 +1,43 @@
+package com.danielbautista.walletapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListAccountActivity extends AppCompatActivity {
+
+    private ArrayList<Account> listAccountRV = new ArrayList<>();
+    private RecyclerView myRecycleAccount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_account);
+        loadFakeData();
+
+        myRecycleAccount = findViewById(R.id.rv_list_account);
+        AccountAdapter myAdaptador = new AccountAdapter(listAccountRV);
+        myRecycleAccount.setAdapter(myAdaptador);
+        myRecycleAccount.setLayoutManager(new LinearLayoutManager(ListAccountActivity.this));
+
+
+    }
+
+
+
+    private void loadFakeData(){
+
+        Account myAccount1 = new Account("Bancolombia", "Cuenta de Ahorros", 11111111.0);
+        Account myAccount2 = new Account("Davivienda", "Cuenta debito", 222222222.0);
+        Account myAccount3 = new Account("Efectivo", "Efectivo", 333333333.0);
+        listAccountRV.add(myAccount1);
+        listAccountRV.add(myAccount2);
+        listAccountRV.add(myAccount3);
+
+
+    }
+}
